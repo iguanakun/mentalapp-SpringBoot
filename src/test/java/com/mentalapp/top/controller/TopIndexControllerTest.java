4f2a476475b1193d32ea0c7722332d447f35698d
iguanakun <yw16027@gmail.com>
2025-10-26 14:39:59 +0900

    TopControllerテスト作成

---
+++ src/test/java/com/mentalapp/top/controller/TopIndexControllerTest.java
@@ -0,0 +1,26 @@
+package com.mentalapp.top.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+public class TopIndexControllerTest {
+  private MockMvc mockMvc;
+  private TopIndexController topIndexController;
+
+  @BeforeEach
+  public void setUp() {
+    topIndexController = new TopIndexController();
+    mockMvc = MockMvcBuilders.standaloneSetup(topIndexController).build();
+  }
+
+  @Test
+  void testShowIndex() throws Exception {
+    mockMvc.perform(get("/")).andExpect(view().name("index")).andExpect(status().isOk());
+  }
+}
